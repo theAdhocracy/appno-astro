c3203e813adcfe4804390a367d0056ba7b49c94c
Murray Champernowne <murrayadcock@gmail.com>
2024-06-03 14:17:06 +0100

    feat: sanity integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,20 @@
-import { defineConfig } from 'astro/config';
+import sanity from "@sanity/astro";
+import { defineConfig } from "astro/config";
+import { loadEnv } from "vite";
+
+const { SANITY_PROJECT_ID, SANITY_DATASET } = loadEnv(
+	process.env.NODE_ENV,
+	process.cwd(),
+	""
+);
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [
+		sanity({
+			projectId: SANITY_PROJECT_ID,
+			dataset: SANITY_DATASET,
+			useCdn: false,
+		}),
+	],
+});
